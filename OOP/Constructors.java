ed79832ff7222b75a9b2c1975bec8d475c010c54
Yogesh Reddy <yogiyogeshreddy1234@gmail.com>
2025-11-15 06:55:47 +0530

    OOP Coding

--- OOP/Constructors.java
+++ OOP/Constructors.java
@@ -2,10 +2,43 @@ package OOP;
 
 import java.util.Scanner;
 
-//All about Constructors
+//All about Constructors + this
 public class Constructors {
+    int rno;
+    String name;
+    float marks=90;
+
+    //default constructor
+    Constructors(){
+        //we need a way to add the values of the above
+        //properties object by object
+
+        //we need one word to access every object using ->>> this << (keyword)
+        this.rno=13;
+        this.name="yogesh";
+        this.marks=100;
+    }
+
+    //parameterized constructor
+    Constructors(int rno,String name,float marks){
+        this.rno=rno;
+        this.name=name;
+        this.marks=marks;
+    }
+
+    void greeting(){
+        System.out.println("Hello! My name is "+ name);
+    }
+
     public static void main(String[]args){
         Scanner sc= new Scanner(System.in);
+        //obj created and called default constructor
+        Constructors obj=new Constructors();
+        obj.greeting();
+
+        //obj created and called parameterized constructor
+        Constructors obj1=new Constructors(1,"S Yogesh Reddy",99);
+        obj1.greeting();
 
     }
 }
---
+++ OOP/Wrapper_cls_eg.java
@@ -0,0 +1,68 @@
+package OOP;
+
+import java.util.Scanner;
+// All about Wrapper class
+public class Wrapper_cls_eg {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        // byte data type
+        byte a = 1;
+
+        // wrapping around Byte object (use valueOf or autoboxing)
+        Byte byteobj = Byte.valueOf(a);
+
+        // int data type
+        int b = 10;
+
+        // wrapping around Integer object (use valueOf or autoboxing)
+        Integer intobj = Integer.valueOf(b);
+
+        // float data type
+        float c = 18.6f;
+
+        // wrapping around Float object (use valueOf or autoboxing)
+        Float floatobj = Float.valueOf(c);
+
+        // double data type
+        double d = 250.5;
+
+        // Wrapping around Double object (use valueOf or autoboxing)
+        Double doubleobj = Double.valueOf(d);
+
+        // char data type
+        char e = 'a';
+
+        // wrapping around Character object (autoboxing)
+        Character charobj = e;
+
+        // printing the values from objects
+        System.out.println(
+                "Values of Wrapper objects (printing as objects)");
+        System.out.println("\nByte object byteobj: "
+                + byteobj);
+        System.out.println("\nInteger object intobj: "
+                + intobj);
+        System.out.println("\nFloat object floatobj: "
+                + floatobj);
+        System.out.println("\nDouble object doubleobj: "
+                + doubleobj);
+        System.out.println("\nCharacter object charobj: "
+                + charobj);
+
+        // objects to data types unwrapping objects to primitive datatypes
+        byte bv = byteobj;
+        int iv = intobj;
+        float fv = floatobj;
+        double dv = doubleobj;
+        char cv = charobj;
+
+        // printing the values from data types
+        System.out.println(
+                "\nUnwrapped values (printing as data types)");
+        System.out.println("\nbyte value, bv: " + bv);
+        System.out.println("\nint value, iv: " + iv);
+        System.out.println("\nfloat value, fv: " + fv);
+        System.out.println("\ndouble value, dv: " + dv);
+        System.out.println("\nchar value, cv: " + cv);
+    }
+}
